3c5adcda97a7af33798bd8bd1a2d5f3ac5c70903
SimaoFernandes <simaopsfernandes100@gmail.com>
2021-06-04 21:15:47 +0100

    BugFix Run.ps1

--- RackIT/run.ps1
+++ RackIT/run.ps1
@@ -2,5 +2,4 @@ Clear-Host
 php artisan migrate:refresh
 php artisan db:seed --class=PermissionTableSeeder
 php artisan db:seed --class=CreateRolesTableSeeder
-php artisan db:seed --class=CreateExamplesSeeder
 php artisan serve
